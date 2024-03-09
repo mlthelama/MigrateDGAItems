7df7d5bb721b07aaba2799b38da9adb77b1d8581
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2024-03-09 17:01:26 -0500

    initial commit

---
+++ MigrateDGAItems/DGAClasses/CustomDGAClasses.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Xml.Serialization;
+using StardewValley;
+using StardewValley.Objects;
+
+namespace MigrateDGAItems.DGAClasses
+{
+    [XmlType("Mods_DGABasicFurniture")]
+    public class CustomBasicFurniture: Furniture
+	{
+		public CustomBasicFurniture()
+		{
+		}
+	}
+
+    [XmlType("Mods_DGABedFurniture")]
+    public class CustomBedFurniture : BedFurniture
+    {
+        public CustomBedFurniture()
+        {
+        }
+    }
+
+    [XmlType("Mods_DGABigCraftable")]
+    public class CustomBigCraftable : StardewValley.Object
+    {
+        public CustomBigCraftable()
+        {
+        }
+    }
+
+    [XmlType("Mods_DGABoots")]
+    public class CustomBoots : Boots
+    {
+        public CustomBoots()
+        {
+        }
+    }
+
+    [XmlType("Mods_DGACrop")]
+    public class CustomCrop : Crop
+    {
+        public CustomCrop()
+        {
+        }
+    }
+
+    [XmlType("Mods_DGAFence")]
+    public class CustomFence : Fence
+    {
+        public CustomFence()
+        {
+        }
+    }
+
+    [XmlType("Mods_DGAFishTankFurniture")]
+    public class CustomFishTankFurniture : FishTankFurniture
+    {
+        public CustomFishTankFurniture()
+        {
+        }
+    }
+}
+
---
+++ MigrateDGAItems/ModEntry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace MigrateDGAItems
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
+        {
+            Utility.ForEachItem(i => { fixItem(i); return true; });
+        }
+
+        private Item fixItem(Item item) {
+            // if it's an error item, try to fix it
+            if (ItemRegistry.GetDataOrErrorItem(item.QualifiedItemId).IsErrorItem)
+            {
+                Monitor.Log($"Error item found with name: {item.Name}", LogLevel.Debug);
+                Item fuzzyResult = Utility.fuzzyItemSearch(item.Name);
+                if (fuzzyResult is null)
+                {
+                    string nameWithoutQualifiers = item.Name.Split(".").Last();
+                    string nameWithoutDGA = item.Name.Split(".").First() + "." + nameWithoutQualifiers;
+                    // Try searching for the item name
+                    fuzzyResult = Utility.fuzzyItemSearch(nameWithoutDGA);
+                    // Give up if that didn't work
+                    if (fuzzyResult is null)
+                    {
+                        fuzzyResult = item;
+                    }
+                }
+                return fuzzyResult;
+            }
+            else
+            {
+                return item;
+            }
+        }
+    }
+}
